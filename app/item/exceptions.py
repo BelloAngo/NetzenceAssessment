f2390f3cf4085e622a0e80b2b128c5a22aacad14
Bello Shehu Ango <angobello0@gmail.com>
2024-11-16 21:27:33 +0100

    chore: implement ItemNotFound exc

---
+++ app/item/exceptions.py
@@ -0,0 +1,10 @@
+from app.common.exceptions import NotFound
+
+
+class ItemNotFound(NotFound):
+    """
+    Exception for 404 Item Not Found
+    """
+
+    def __init__(self, *, loc=None):
+        super().__init__("Item Not Found", loc=loc)
